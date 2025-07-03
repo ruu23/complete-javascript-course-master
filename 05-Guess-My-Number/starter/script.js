be259bbc5049a4110c1629aab7ec95f5fe1ddfde
Arwa Mahmoud <arwamah777@gmail.com>
2025-07-03 10:54:29 +0300

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = finction (message) =
+document.querySelector('.message').textContent = message
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // conditions of guess values
+  if (!guess) {
+    displayMessage('⛔ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈High number!' : '📈low number!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  displayMessage('Start guessing...');
+
+  document.body.style.backgroundColor = '#222';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.number').style.width = '15rem';
+});
